f145e9dfbdba332411d54be58c889abff6b29009
Gaurav <gauravv2504@gmail.com>
2026-08-02 16:40:27 +0530

    chore: remove unused WidgetError and RendererError exceptions

--- src/profileforge/core/exceptions.py
+++ src/profileforge/core/exceptions.py
@@ -10,12 +10,7 @@ class ThemeError(ProfileForgeError):
     """Raised when a theme fails to load or parse"""
 
 
-class WidgetError(ProfileForgeError):
-    """Raised when a widget encounters an error during rendering or loading"""
 
-
-class RendererError(ProfileForgeError):
-    """Raised when the layout engine or renderer fails"""
 
 
 class ConnectorError(ProfileForgeError):
