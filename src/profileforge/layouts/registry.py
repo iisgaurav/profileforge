2e236509042de58074f111520459530b5de0f5e6
Gaurav <gauravv2504@gmail.com>
2026-08-02 16:37:42 +0530

    chore: remove dead legacy code (layouts and dashboard) from old architecture

--- src/profileforge/layouts/registry.py
+++
@@ -1,6 +0,0 @@
-from profileforge.layouts.base import DashboardLayout
-from profileforge.layouts.bento import BentoLayout
-
-LAYOUT_REGISTRY: dict[str, type[DashboardLayout]] = {
-    "bento": BentoLayout,
-}
